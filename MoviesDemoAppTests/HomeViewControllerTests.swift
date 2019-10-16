7932ca37d5d9654464217dc4d8976ec67fda4459
Sudhakar Reddy <sudhakarmallidi@gmail.com>
2019-10-16 09:16:46 +0800

    HomeVC cases added

--- MoviesDemoAppTests/HomeViewControllerTests.swift
+++ MoviesDemoAppTests/HomeViewControllerTests.swift
@@ -55,29 +55,4 @@ class HomeViewControllerTests: XCTestCase {
             }
         }
     }
-    func testURLEncoding() {
-        guard let url = URL(string: "https:www.google.com/") else {
-            XCTAssertTrue(false, "Could not instantiate url")
-            return
-        }
-        var urlRequest = URLRequest(url: url)
-        let parameters: Parameters = [
-            "UserID": 1,
-            "Name": "Chandresh",
-            "Email": "chandresh@gmail.com",
-            "IsCool": true
-        ]
-        do {
-            let encoder = URLParameterEncoder()
-            try encoder.encode(urlRequest: &urlRequest, with: parameters)
-            guard let fullURL = urlRequest.url else {
-                XCTAssertTrue(false, "urlRequest url is nil.")
-                return
-            }
-            let expectedURL = "https:www.google.com/?Name=Chandresh&Email=chandresh%2540gmail.com&UserID=1&IsCool=true"
-            XCTAssertEqual(fullURL.absoluteString.sorted(), expectedURL.sorted())
-        } catch {
-            
-        }
-    }
 }
